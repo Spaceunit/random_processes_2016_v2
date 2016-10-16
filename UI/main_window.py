6d7c9f91d9538a54d2fe0fd3485d2c1bc1159196
Oleksiy <oleksiypolshchak@gmail.com>
2016-10-16 16:06:12 +0300

    Next initial commit

--- UI/main_window.py
+++ UI/main_window.py
@@ -3,6 +3,45 @@ from json import load, JSONDecodeError
 from math import pi
 
 from PyQt5.QtGui import QIntValidator, QDoubleValidator
-from PyQt5.QtWidgets import (QWidget, QApplication, QPushButton, QMainWindow, QTabWidget, QVBoxLayout, QLabel,
+from PyQt5.QtWidgets import (QWidget, QTextEdit, QAction, QApplication, QPushButton, QMainWindow, QTabWidget, QVBoxLayout, QLabel,
                              QGridLayout, QLineEdit, QFileDialog, QMessageBox)
+from PyQt5.QtGui import QIcon
 from matplotlib import pyplot
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exitAction = QAction(QIcon('icon/ic_exit_to_app_black_48dp_2x.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q in Linux or Command+Q in MacOS')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Main window')
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
